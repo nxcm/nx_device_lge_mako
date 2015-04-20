702cb2b9b2b32f2cdc1ffa01f33c6f7b1a290aa7
AragaoAnderson <andersonaragao@me.com>
2015-04-19 23:14:31 -0300

    Mako: Remove more AOSP stuff
    Change-Id: I99aea5801735303761eb218990ee46b1f1ba0e1f

--- device.mk
+++ device.mk
@@ -41,9 +41,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-        LiveWallpapers \
         LiveWallpapersPicker \
-        VisualizationWallpapers \
         librs_jni
 
 PRODUCT_COPY_FILES += \
